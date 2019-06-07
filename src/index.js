48845e6441700c190dc269c995cd0677e8d58b51
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-06-07 22:45:55 +0530

    :sparkles: Added lazy loading and react epic spinners

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import Sgpa from './components/sgpa'
+import { FingerprintSpinner } from 'react-epic-spinners'
+
+const Sgpa = lazy(() => import('./components/sgpa'))
+const Cgpa = lazy(() => import('./components/CGPA'))
+
+const loading = (
+    <FingerprintSpinner className="loading" color="#fff" />
+)
 
 ReactDOM.render(
-    <Router>
-        <React.Fragment>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route exact path="/sgpa" component={Sgpa} />
-            </Switch>
-        </React.Fragment>
-    </Router>, 
+    <Suspense fallback={loading}>
+        <Router>
+            <React.Fragment>
+                <Switch>
+                    <Route exact path="/" component={(props) => <App {...props} />} />
+                    <Route exact path="/sgpa" component={(props) => <Sgpa {...props} />} />
+                    <Route exact path="/cgpa" component={(props) => <Cgpa {...props} />} />
+                </Switch>
+            </React.Fragment>
+        </Router>
+    </Suspense>, 
     document.getElementById('root')
 )
 
